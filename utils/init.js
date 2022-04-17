fbcc56295a8f18f9158409696e98b0396535777a
Max Klammer <maximilian.klammer@gmail.com>
2022-04-17 13:35:46 +0200

    increased version

--- utils/init.js
+++ utils/init.js
@@ -8,7 +8,7 @@ export default ({ clear = true }) => {
 		tagLine: `by Max Klammer`,
 		description:
 			'Creates a new MDX file and frontmatter header for my blog',
-		version: `0.0.6`,
+		version: `0.0.7`,
 		bgColor: '#36BB09',
 		color: '#000000',
 		bold: true,
